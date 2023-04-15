d665251d674932ad9b8a383a12111abdf2552a14
Zrazhevskii <vbn-134@yandex.ru>
2023-04-15 10:56:57 +0300

    коррекция функции

--- homework4/check_password.js
+++ homework4/check_password.js
@@ -6,11 +6,11 @@ const rl = require('readline').createInterface(process.stdin, process.stdout);
 function getPasswordChecker(password) {
     function wrapper() {
         rl.question('Введите пароль: ', (past) => {
-            console.log('Вы ввели: ', past);
             if (past == password) {
+                console.log('Отлично угадали!')
                 rl.close();
-                return
             } else {
+                console.log('Вы ввели не правильный пароль')
                 return wrapper();
             }
         })
@@ -18,21 +18,21 @@ function getPasswordChecker(password) {
     return wrapper;
 }
 
-// const check = getPasswordChecker('qwerty');
-// check()
-
-
-function getPasswordChecker(password) {
-    return function(past) {
-        if (password === past) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
 const check = getPasswordChecker('qwerty');
+check()
+
+
+// function getPasswordChecker(password) {
+//     return function(past) {
+//         if (password === past) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+// }
+
+// const check = getPasswordChecker('qwerty');
 
 // console.log(check('qwe'));
 // console.log(check('qwerty'));
